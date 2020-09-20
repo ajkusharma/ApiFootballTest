05ca5ee7a6d9ec59e97fe355b364586b812789ec
ajkusharma <47526191+ajkusharma@users.noreply.github.com>
2020-09-20 23:23:36 +0530

    Update ApifootballApplicationTests.java

--- src/test/java/com/poc/apifootball/apifootball/ApifootballApplicationTests.java
+++ src/test/java/com/poc/apifootball/apifootball/ApifootballApplicationTests.java
@@ -1,13 +1,27 @@
 package com.poc.apifootball.apifootball;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApifootballApplicationTests {
+	
+	@Autowired 
+	ApifootballController apiFootballController;
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void testGetId() throws IOException {
+		
+		Country country = new Country();
+		apiFootballController.getId(1L);
+		
 	}
 
 }
